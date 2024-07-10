65fbf293debd362aef9ce83ad812bd2418352d8c
Gorka Eraña <eranagorka@gmail.com>
2024-07-10 14:47:22 +0200

    Refactored 'expressions.rs'
    Better
    - Rust hygiene (I think?)
    - naming conventions

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,4 +1,4 @@
-use polars::prelude::*;
+use polars::prelude::{DataType::String, IntoSeries, PolarsResult, PolarsError::InvalidOperation, Series};
 use pyo3_polars::derive::polars_expr;
 use std::path::Path;
 
@@ -8,16 +8,16 @@ use polars_core::datatypes::{BooleanChunked, StringChunked};
 fn exists(inputs: &[Series]) -> PolarsResult<Series>{
     let s = inputs.get(0).expect("no series received");
     match s.dtype() {
-	DataType::String => {
-	    let ca = s.str()?;
-	    let out: BooleanChunked = ca.apply_generic(
+	String => {
+	    let chunked_array = s.str()?;
+	    let out: BooleanChunked = chunked_array.apply_generic(
 		|value: Option<&str>| -> Option<bool> {
-		    Some(value.map_or(false, |s| Path::new(s).exists()))
-		}
+		    value.map(|s| Path::new(s).exists())
+		}	
 	    );
 	    Ok(out.into_series())
 	}
-	_ => Err(PolarsError::InvalidOperation("exists only works on string data".into()))
+	_ => Err(InvalidOperation(format!("'exists' only works on string data, instead got {}", s.dtype()).into()))
     }
 }
 
@@ -25,16 +25,16 @@ fn exists(inputs: &[Series]) -> PolarsResult<Series>{
 fn is_absolute(inputs: &[Series]) -> PolarsResult<Series>{
     let s = inputs.get(0).expect("no series received");
     match s.dtype() {
-	DataType::String => {
-	    let ca = s.str()?;
-	    let out: BooleanChunked = ca.apply_generic(
+	String => {
+	    let chunked_array = s.str()?;
+	    let out: BooleanChunked = chunked_array.apply_generic(
 		|value: Option<&str>| -> Option<bool> {
-		    Some(value.map_or(false, |s| Path::new(s).is_absolute()))
+		    value.map(|s| Path::new(s).is_absolute())
 		}
 	    );
 	    Ok(out.into_series())
 	}
-	_ => Err(PolarsError::InvalidOperation("is_absolute only works on string data".into()))
+	_ => Err(InvalidOperation(format!("'is_absolute' only works on string data, instead got {}", s.dtype()).into()))
     }
 }
 
@@ -42,16 +42,16 @@ fn is_absolute(inputs: &[Series]) -> PolarsResult<Series>{
 fn is_dir(inputs: &[Series]) -> PolarsResult<Series>{
     let s = inputs.get(0).expect("no series received");
     match s.dtype() {
-	DataType::String => {
-	    let ca = s.str()?;
-	    let out: BooleanChunked = ca.apply_generic(
+	String => {
+	    let chunked_array = s.str()?;
+	    let out: BooleanChunked = chunked_array.apply_generic(
 		|value: Option<&str>| -> Option<bool> {
-		    Some(value.map_or(false, |s| Path::new(s).is_dir()))
+		    value.map(|s| Path::new(s).is_dir())
 		}
 	    );
 	    Ok(out.into_series())
 	}
-	_ => Err(PolarsError::InvalidOperation("is_dir only works on string data".into()))
+	_ => Err(InvalidOperation(format!("'is_dir' only works on string data, instead got {}", s.dtype()).into()))
     }
 }
 
@@ -59,39 +59,65 @@ fn is_dir(inputs: &[Series]) -> PolarsResult<Series>{
 fn is_file(inputs: &[Series]) -> PolarsResult<Series>{
     let s = inputs.get(0).expect("no series received");
     match s.dtype() {
-	DataType::String => {
-	    let ca = s.str()?;
-	    let out: BooleanChunked = ca.apply_generic(
+	String => {
+	    let chunked_array = s.str()?;
+	    let out: BooleanChunked = chunked_array.apply_generic(
 		|value: Option<&str>| -> Option<bool> {
-		    Some(value.map_or(false, |s| Path::new(s).is_file()))
+		    value.map(|s| Path::new(s).is_file())
 		}
 	    );
 	    Ok(out.into_series())
 	}
-	_ => Err(PolarsError::InvalidOperation("is_file only works on string data".into()))
+	_ => Err(InvalidOperation(format!("'is_file' only works on string data, instead got {}", s.dtype()).into()))
     }
 }
+
+#[polars_expr(output_type=String)]
+fn name(inputs: &[Series]) -> PolarsResult<Series>{
+    let s = inputs.get(0).expect("no series received");
+    match s.dtype() {
+	String => {
+	    let chunked_array = s.str()?;
+	    let out: StringChunked = chunked_array.apply_generic(
+		|value: Option<&str>| -> Option<&str> {
+		    value.map(
+			|s| {
+			    Path::new(s).file_name()
+				.expect("a path ought to have a parent")
+				.to_str()
+				.expect("a path's parent ought to be able to be converted to a string")
+		    }
+		    )
+		}
+	    );
+	    Ok(out.into_series())
+	}
+	_ => Err(InvalidOperation(format!("'name' only works on string data, instead got {}", s.dtype()).into()))
+    }
+}
+
 
 #[polars_expr(output_type=String)]
 fn parent(inputs: &[Series]) -> PolarsResult<Series>{
     let s = inputs.get(0).expect("no series received");
     match s.dtype() {
-	DataType::String => {
-	    let ca = s.str()?;
-	    let out: StringChunked = ca.apply_generic(
+	String => {
+	    let chunked_array = s.str()?;
+	    let out: StringChunked = chunked_array.apply_generic(
 		|value: Option<&str>| -> Option<&str> {
-		    Some(value.map_or("", |s| {
-			Path::new(s).parent()
-			    .expect("a path ought to have a parent")
-			    .to_str()
-			    .expect("a path ought to be able to be converted to a string")
-		    }
-		    )
+		    value.map(
+			|s| {
+			    Path::new(s).parent()
+				.expect("a path ought to have a parent")
+				.to_str()
+				.expect("a path ought to be able to be converted to a string")
+			}
 		    )
 		}
 	    );
 	    Ok(out.into_series())
 	}
-	_ => Err(PolarsError::InvalidOperation("parent only works on string data".into()))
+	_ => Err(InvalidOperation(format!("'parent' only works on string data, instead got {}", s.dtype()).into()))
     }
 }
+
